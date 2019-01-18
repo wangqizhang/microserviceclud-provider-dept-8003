bb02bfd96d0ee356f164eb42f79070990243dbc3
wangqizhang <wangqizhang@qq.com>
2019-01-18 13:15:31 +0800

    commit hystrix 8001

---
+++ microservicecloud-provider-dept-hystrix-8001/src/main/java/com/atguigu/springcloud/DeptProvider8001_App.java
@@ -0,0 +1,19 @@
+package com.atguigu.springcloud;
+ 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+ 
+@SpringBootApplication
+@EnableEurekaClient
+@EnableCircuitBreaker
+public class DeptProvider8001_App
+{
+  public static void main(String[] args)
+  {
+   SpringApplication.run(DeptProvider8001_App.class, args);
+  }
+}
+ 
+
